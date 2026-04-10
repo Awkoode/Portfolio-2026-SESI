01deb0c1facf8c4858ba7aa7143b673481999063
unknown <arthur.awkologeski@gmail.com>
2026-04-10 00:37:17 -0300

    SAVE 10/4 PGNCSS

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const body = document.body;
 
 botaoTema.addEventListener("click", () => {
   body.classList.toggle("claro");
+});
 
-  if (body.classList.contains("claro")) {
-    botaoTema.textContent = "☀️";
-  } else {
-    botaoTema.textContent = "🌙";
-  }
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('ativo');
+  menu.classList.toggle('aberto');
+  navbar.classList.toggle('expandida');
+});
+
+menu.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('ativo');
+    menu.classList.remove('aberto');
+    navbar.classList.remove('expandida');
+  });
 });
